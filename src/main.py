ace22aaa783b025834f8def462a47e20507a8b88
aosoriogo <aosoriogo@unal.edu.co>
2026-05-02 21:46:14 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
 # AQUÍ VA LO NUEVO (Entrega 2)
 # Solo el MENÚ y llamadas a funciones
+from logica import cargar_dataset # Importas tu función
+
+# 1. Cargar los datos al inicio (Dataset completo de 200+)
+mis_datos = cargar_dataset("Data/diabetes_COMPLETO.csv")
+
+# 2. Ahora 'mis_datos' es una lista de diccionarios que puedes usar en el menú
+while True:
+    print("1. Buscar")
+    print("2. Estadísticas")
+    print("3. Guardar resultados (F1)")
